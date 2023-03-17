9fd46b1242a646474ce4e857d737bd0751a846f0
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-03-17 16:47:29 -0400

    commit tree 145124ee03214759a7776e4ffd1b5eebe4451a9f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves dwarves# code an argument here
+  dwarves.each_with_index do |d, i|
+    puts "#{i+1}. #{d}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers#["earth", "wind", "fire", "water", "heart"]
+  planeteers.map { |p| p.capitalize << '!'} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls#["puff", "go", "two"]
+  calls.any? { |elem| elem.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese foods# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found_index = foods.index do |food|
+    cheese_types.index(food)
+  end
+  # binding.pry
+  found_index ? foods[found_index] : found_index
 end
